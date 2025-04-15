f04e6ee338337da9e0420c71f0bd008007fe8d6d
Sergey Astapov <sergeastapov@gmail.com>
2025-04-14 23:14:33 -0400

    Convert to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { lookupValidator as default } from 'validated-changeset';
-export { setMessages } from './utils/messages.js';
+export { setMessages } from './utils/messages.ts';
+
+export type { ValidatorMapFunc } from 'validated-changeset/types';
